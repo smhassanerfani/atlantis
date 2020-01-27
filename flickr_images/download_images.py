5ee813478673883c91b38b2752f748f5816ce903
serfani <serfani@email.sc.edu>
2020-01-27 15:36:01 -0500

    Modified download_images.py, 'license', 'flickr_url', 'file_name', 'date_captured' were added.

--- flickr_images/download_images.py
+++ flickr_images/download_images.py
@@ -8,12 +8,13 @@ import os
 import csv
 import json
 import requests
+from datetime import datetime
 
 # Key:
 api_key = 'da03d4d2c9d70753c5b918009711b9ea'
 
 # Secret:
-secret = 'f35c6f99121e3e83' 
+secret = 'f35c6f99121e3e83'
 
 natural_list = ['flood', 'river']
 artificial_list = ['dam', 'canal']
@@ -21,7 +22,7 @@ artificial_list = ['dam', 'canal']
 #natural_list = list()
 #artificial_list = list()
 
-#with open('list.csv', encoding="utf8", errors='ignore') as csvfile:
+# with open('list.csv', encoding="utf8", errors='ignore') as csvfile:
 #    list_csv_dict = csv.DictReader(csvfile)
 #    for row in list_csv_dict:
 #        natural_list.append(row.get('natural'))
@@ -29,17 +30,25 @@ artificial_list = ['dam', 'canal']
 #natural_list = list(filter(None, natural_list))
 
 licenses = [
-    {"url": "http://creativecommons.org/licenses/by-nc-sa/2.0/","id": 1,"name": "Attribution-NonCommercial-ShareAlike License"},
-    {"url": "http://creativecommons.org/licenses/by-nc/2.0/","id": 2,"name": "Attribution-NonCommercial License"},
-    {"url": "http://creativecommons.org/licenses/by-nc-nd/2.0/","id": 3,"name": "Attribution-NonCommercial-NoDerivs License"},
-    {"url": "http://creativecommons.org/licenses/by/2.0/","id": 4,"name": "Attribution License"},
-    {"url": "http://creativecommons.org/licenses/by-sa/2.0/","id": 5,"name": "Attribution-ShareAlike License"},
-    {"url": "http://creativecommons.org/licenses/by-nd/2.0/","id": 6,"name": "Attribution-NoDerivs License"},
-    {"url": "http://flickr.com/commons/usage/","id": 7,"name": "No known copyright restrictions"},
-    {"url": "http://www.usa.gov/copyright.shtml","id": 8,"name": "United States Government Work"}
-    ]
+    {"url": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
+        "id": 1, "name": "Attribution-NonCommercial-ShareAlike License"},
+    {"url": "http://creativecommons.org/licenses/by-nc/2.0/",
+        "id": 2, "name": "Attribution-NonCommercial License"},
+    {"url": "http://creativecommons.org/licenses/by-nc-nd/2.0/",
+        "id": 3, "name": "Attribution-NonCommercial-NoDerivs License"},
+    {"url": "http://creativecommons.org/licenses/by/2.0/",
+        "id": 4, "name": "Attribution License"},
+    {"url": "http://creativecommons.org/licenses/by-sa/2.0/",
+        "id": 5, "name": "Attribution-ShareAlike License"},
+    {"url": "http://creativecommons.org/licenses/by-nd/2.0/",
+        "id": 6, "name": "Attribution-NoDerivs License"},
+    {"url": "http://flickr.com/commons/usage/", "id": 7,
+        "name": "No known copyright restrictions"},
+    {"url": "http://www.usa.gov/copyright.shtml",
+        "id": 8, "name": "United States Government Work"}
+]
 
-os.mkdir('images')   
+os.mkdir('images')
 main_dir = os.path.join(os.getcwd(), 'images')
 
 for dir1 in natural_list:
@@ -47,31 +56,38 @@ for dir1 in natural_list:
         file_path = os.path.join(dir1, licenses[num].get('name'))
         file_path2 = os.path.join(main_dir, file_path)
         os.makedirs(file_path2)
-        
+
         tag = dir1
         license_type = licenses[num].get('id')
         per_page = 3
         page_number = 1
-        
-        url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key={api_key}&tags={tag}&license={license_type}&is_commons=&per_page={per_page}&page={page_number}&format=json&nojsoncallback=1'.format(api_key=api_key, tag=tag, license_type=license_type, per_page=per_page, page_number=page_number)
+
+        url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key={api_key}&tags={tag}&license={license_type}&is_commons=&per_page={per_page}&page={page_number}&format=json&nojsoncallback=1'.format(
+            api_key=api_key, tag=tag, license_type=license_type, per_page=per_page, page_number=page_number)
         r = requests.get(url)
         data = r.json()
-        
+
         for element in data['photos']['photo']:
             del element['title'], element['owner'], element['ispublic'], element['isfriend'], element['isfamily']
+            element['license'] = licenses[num].get('id')
+            element['flickr_url'] = 'https://farm{farm_id}.staticflickr.com/{server_id}/{id}_{secret}_z.jpg'.format(
+                farm_id=element['farm'], server_id=element['server'], id=element['id'], secret=element['secret'])
+            element['file_name'] = str(element['id']) + '.jpg'
+            element['date_captured'] = datetime.now(
+            ).strftime("%m/%d/%Y, %H:%M:%S")
 
         with open(os.path.join(file_path2, 'json_file.json'), 'w') as tf:
             json.dump(data, tf, indent=2)
-        
-        
+
         for element in data['photos']['photo']:
             try:
-                img_lnk = requests.get('https://farm{farm_id}.staticflickr.com/{server_id}/{id}_{secret}_z.jpg'.format(farm_id=element['farm'], server_id=element['server'], id=element['id'], secret=element['secret']))
-                with open(os.path.join(file_path2, str(element) + '.jpg'), 'wb') as tf2:
+                img_lnk = requests.get('https://farm{farm_id}.staticflickr.com/{server_id}/{id}_{secret}_z.jpg'.format(
+                    farm_id=element['farm'], server_id=element['server'], id=element['id'], secret=element['secret']))
+                with open(os.path.join(file_path2, element['file_name']), 'wb') as tf2:
                     tf2.write(img_lnk.content)
             except OSError:
                 print('File name too long')
-            except (requests.ConnectTimeout,requests.HTTPError, requests.ReadTimeout, requests.Timeout, requests.ConnectionError):
+            except (requests.ConnectTimeout, requests.HTTPError, requests.ReadTimeout, requests.Timeout, requests.ConnectionError):
                 print(' Max retries exceeded with url')
 
 
