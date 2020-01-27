418eead80c8aefaf9d7872c7168049954b268336
serfani <serfani@email.sc.edu>
2020-01-27 16:02:06 -0500

    Modified download_images.py, images size and comments were added to file.

--- flickr_images/download_images.py
+++ flickr_images/download_images.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  8 17:03:41 2020
-
-@author: SERFANI
-"""
 import os
 import csv
 import json
@@ -16,18 +10,17 @@ api_key = 'da03d4d2c9d70753c5b918009711b9ea'
 # Secret:
 secret = 'f35c6f99121e3e83'
 
-natural_list = ['flood', 'river']
-artificial_list = ['dam', 'canal']
+natural_list = list()
+artificial_list = list()
 
-#natural_list = list()
-#artificial_list = list()
+with open('list.csv', encoding="utf8", errors='ignore') as csvfile:
+    list_csv_dict = csv.DictReader(csvfile)
+    for row in list_csv_dict:
+        natural_list.append(row.get('natural'))
+        artificial_list.append(row.get('artificial'))
 
-# with open('list.csv', encoding="utf8", errors='ignore') as csvfile:
-#    list_csv_dict = csv.DictReader(csvfile)
-#    for row in list_csv_dict:
-#        natural_list.append(row.get('natural'))
-#        artificial_list.append(row.get('artificial'))
-#natural_list = list(filter(None, natural_list))
+natural_list = list(filter(None, natural_list))
+artificial_list = list(filter(None, artificial_list))
 
 licenses = [
     {"url": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
@@ -67,6 +60,32 @@ for dir1 in natural_list:
         r = requests.get(url)
         data = r.json()
 
+        ''' we need to pars the information that we got from flickr in such a format:
+            This is what we have:
+
+            {
+                "id": "49379823277",
+                "owner": "82256086@N00",
+                "secret": "d2afbf2523",
+                "server": "65535",
+                "farm": 66,
+                "title": "Hudson River Sunset",
+                "ispublic": 1,
+                "isfriend": 0,
+                "isfamily": 0
+            }
+
+            This is what we want:
+            {
+                "id": 229753
+                "license": 4,
+                "flickr_url": "http://farm4.staticflickr.com/3271/2787713866_34ab4ca3d3_z.jpg",
+                "width": 640,
+                "height": 427,
+                "file_name": "000000229753.jpg",
+                "date_captured": "2013-11-17 00:12:59",
+            }
+        '''
         for element in data['photos']['photo']:
             del element['title'], element['owner'], element['ispublic'], element['isfriend'], element['isfamily']
             element['license'] = licenses[num].get('id')
@@ -76,9 +95,25 @@ for dir1 in natural_list:
             element['date_captured'] = datetime.now(
             ).strftime("%m/%d/%Y, %H:%M:%S")
 
+            # "flickr.photos.getSizes" method to add width and length information of the images
+            url2 = 'https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key={api_key}&photo_id={photo_id}&format=json&nojsoncallback=1'.format(
+                api_key=api_key, photo_id=element['id'])
+            r2 = requests.get(url2)
+            data2 = r2.json()
+            for dictionary in data2['sizes']['size']:
+                if dictionary['label'] == "Medium 640":
+                    element['width'] = int(dictionary['width'])
+                    element['height'] = int(dictionary['height'])
+                    break
+                else:
+                    element['width'] = 640
+                    element['height'] = 480
+
+        # dump a json file
         with open(os.path.join(file_path2, 'json_file.json'), 'w') as tf:
             json.dump(data, tf, indent=2)
 
+        # save the images as "id.jpg"
         for element in data['photos']['photo']:
             try:
                 img_lnk = requests.get('https://farm{farm_id}.staticflickr.com/{server_id}/{id}_{secret}_z.jpg'.format(
@@ -89,38 +124,3 @@ for dir1 in natural_list:
                 print('File name too long')
             except (requests.ConnectTimeout, requests.HTTPError, requests.ReadTimeout, requests.Timeout, requests.ConnectionError):
                 print(' Max retries exceeded with url')
-
-
-''' we need to pars the information that we got from flickr in such a format:       
-{
-    "id": 229753
-    "license": 4,
-    "flickr_url": "http://farm4.staticflickr.com/3271/2787713866_34ab4ca3d3_z.jpg",
-    "width": 640,
-    "height": 427,
-    "file_name": "000000229753.jpg",
-    "date_captured": "2013-11-17 00:12:59",
-}
-{
-  "photos": {
-    "page": 1,
-    "pages": 123934,
-    "perpage": 1,
-    "total": "123934",
-    "photo": [
-      {
-        "id": "49379823277",
-        "owner": "82256086@N00",
-        "secret": "d2afbf2523",
-        "server": "65535",
-        "farm": 66,
-        "title": "Hudson River Sunset",
-        "ispublic": 1,
-        "isfriend": 0,
-        "isfamily": 0
-      }
-    ]
-  },
-  "stat": "ok"
-}
-'''
