077612d78573b7101593431ee4c09ef81ef12cbe
serfani <smhassan.erfani@gmail.com>
2021-02-23 14:50:16 -0800

    Modified pipeline fo new color2id.py.

--- flickr/labels_counter/pipeline.sh
+++ flickr/labels_counter/pipeline.sh
@@ -3,11 +3,10 @@
 venv="/home/serfani/Documents/venv/bin/activate";
 source ${venv};
 
-dir="/home/serfani/Downloads/appendix/raw";
+dir="/home/serfani/Desktop/atlantis_analysis";
 sdirs=$(echo $(ls ${dir}));
 
 for sdir in ${sdirs}; do
 	python color2id.py ${dir}/${sdir};
-	python labels_counter.py ${dir}/${sdir};
-
+	
 done
