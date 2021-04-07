543cb8b3a2d8fef3d2457d7cda923cf8030b77da
serfani <serfani@email.sc.edu>
2021-04-07 14:09:38 -0400

    Finilized ATeX analysis.

--- iWERS/classifiers/k_nearest_neighbor.py
+++ iWERS/classifiers/k_nearest_neighbor.py
@@ -21,7 +21,7 @@ class KNearestNeighbor(object):
     self.X_train = X
     self.y_train = y
 
-  def predict(self, X, k=1, num_loops=0):
+  def predict(self, X, k=1, method=0):
     """
     Predict labels for test data using this classifier.
 
@@ -29,42 +29,45 @@ class KNearestNeighbor(object):
     - X: A numpy array of shape (num_test, D) containing test data consisting
          of num_test samples each of dimension D.
     - k: The number of nearest neighbors that vote for the predicted labels.
-    - num_loops: Determines which implementation to use to compute distances
+    - method: Determines which implementation to use to compute distances
       between training points and testing points.
 
     Returns:
     - y: A numpy array of shape (num_test,) containing predicted labels for the
       test data, where y[i] is the predicted label for the test point X[i].
     """
-    if num_loops == 0:
-      dists = self.compute_distances_no_loops(X)
-    elif num_loops == 1:
-      dists = self.compute_distances_one_loop(X)
-    elif num_loops == 2:
-      dists = self.compute_distances_two_loops(X)
+    if (method == "kld" or method == 0):
+      dists = self.compute_distances_kld(X)
+    elif (method == "chi2" or method == 1):
+      dists = self.compute_distances_chi2(X)
+    elif (method == "llv" or method == 2):
+      dists = self.compute_distances_llv(X)
+    elif (method == "l2norm" or method == 3):
+      dists = self.compute_distances_l2norm(X)
     else:
-      raise ValueError('Invalid value %d for num_loops' % num_loops)
+      raise ValueError('Invalid value %d for method' % method)
 
     return self.predict_labels(dists, k=k)
 
-  def kullback_leibler_divergence(self, p, q):
-    p = np.asarray(p)
-    q = np.asarray(q)
-    # filt = np.logical_and(p != 0, q != 0)
-    return np.sum(p * np.log2(p / q), axis=1)
+  # def kullback_leibler_divergence(self, p, q):
+  #   p = np.asarray(p)
+  #   q = np.asarray(q)
+  #   # filt = np.logical_and(p != 0, q != 0)
+  #   return np.sum(p * np.log2(p / q), axis=1)
 
-  def chi_square_statistics(self, p, q):
-    p = np.asarray(p)
-    q = np.asarray(q)
-    return np.sum(((p - q) ** 2) / (p + q), axis=1)
+  # def chi_square_statistics(self, p, q):
+  #   p = np.asarray(p)
+  #   q = np.asarray(q)
+  #   return np.sum(((p - q) ** 2) / (p + q), axis=1)
 
-  def log_likelihood_statistics(self, p, q):
-    p = np.asarray(p)
-    q = np.asarray(q)
-    return - np.sum((p * np.log2(q)), axis=1)
+  # def log_likelihood_statistics(self, p, q):
+  #   p = np.asarray(p)
+  #   q = np.asarray(q)
+  #   return - np.sum((p * np.log2(q)), axis=1)
 
-  def compute_distances_two_loops(self, X):
+  def compute_distances_kld(self, X):
     """
+    kullback_leibler_divergence
     Compute the distance between each test point in X and each training point
     in self.X_train using a nested loop over both the training data and the 
     test data.
@@ -79,43 +82,28 @@ class KNearestNeighbor(object):
     """
     num_test = X.shape[0]
     num_train = self.X_train.shape[0]
+
     dists = np.zeros((num_test, num_train))
+
     n_bins = int(X.max() + 1)
+
     X = np.apply_along_axis(lambda x: np.histogram(
         x, density=True, bins=n_bins, range=(0, n_bins))[0], 1, X)
-    X += 0.000001
     X_train = np.apply_along_axis(lambda x: np.histogram(
         x, density=True, bins=n_bins, range=(0, n_bins))[0], 1, self.X_train)
-    X_train += 0.000001
-    # with np.errstate(divide='ignore', invalid='ignore'):
-    for i in range(num_test):
-      dists[i, :] = self.chi_square_statistics(X[i], X_train)
-      # print(f"dists[{i}]")
-    # print(dists)
-    # for i in range(num_test):
-    #   for j in range(num_train):
-    #     #####################################################################
-    #     # TODO:                                                             #
-    #     # Compute the l2 distance between the ith test point and the jth    #
-    #     # training point, and store the result in dists[i, j]. You should   #
-    #     # not use a loop over dimension.                                    #
-    #     #####################################################################
-    #     n_bins = int(X[i].max() + 1)
-    #     hist, _ = np.histogram(
-    #         X[i], density=True, bins=n_bins, range=(0, n_bins))
-    #     ref_hist, _ = np.histogram(
-    #         self.X_train[j], density=True, bins=n_bins, range=(0, n_bins))
-    #     dists[i][j] = self.kullback_leibler_divergence(hist, ref_hist)
-    #     print(dists[i][j])
-    #     exit()
 
-      # dists[i][j] = np.sqrt(np.sum((X[i] - self.X_train[j])**2))
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    X += 0.000001
+    X_train += 0.000001
+
+    X = np.asarray(X)
+    X_train = np.asarray(X_train)
+
+    for i in range(num_test):
+      dists[i, :] = np.sum(X[i] * np.log2(X[i] / X_train), axis=1)
+
     return dists
 
-  def compute_distances_one_loop(self, X):
+  def compute_distances_chi2(self, X):
     """
     Compute the distance between each test point in X and each training point
     in self.X_train using a single loop over the test data.
@@ -124,20 +112,66 @@ class KNearestNeighbor(object):
     """
     num_test = X.shape[0]
     num_train = self.X_train.shape[0]
+
     dists = np.zeros((num_test, num_train))
+
+    n_bins = int(X.max() + 1)
+
+    X = np.apply_along_axis(lambda x: np.histogram(
+        x, density=True, bins=n_bins, range=(0, n_bins))[0], 1, X)
+    X_train = np.apply_along_axis(lambda x: np.histogram(
+        x, density=True, bins=n_bins, range=(0, n_bins))[0], 1, self.X_train)
+
+    X += 0.000001
+    X_train += 0.000001
+
+    X = np.asarray(X)
+    X_train = np.asarray(X_train)
+
     for i in range(num_test):
-      #######################################################################
-      # TODO:                                                               #
-      # Compute the l2 distance between the ith test point and all training #
-      # points, and store the result in dists[i, :].                        #
-      #######################################################################
-      dists[i, :] = np.sqrt(np.sum((X[i] - self.X_train) ** 2, axis=1))
-      #######################################################################
-      #                         END OF YOUR CODE                            #
-      #######################################################################
+      dists[i, :] = np.sum(((X[i] - X_train) ** 2) / (X[i] + X_train), axis=1)
+
     return dists
 
-  def compute_distances_no_loops(self, X):
+  def compute_distances_llv(self, X):
+    """
+    log_likelihood_statistics
+    Compute the distance between each test point in X and each training point
+    in self.X_train using a nested loop over both the training data and the 
+    test data.
+
+    Inputs:
+    - X: A numpy array of shape (num_test, D) containing test data.
+
+    Returns:
+    - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
+      is the Euclidean distance between the ith test point and the jth training
+      point.
+    """
+    num_test = X.shape[0]
+    num_train = self.X_train.shape[0]
+
+    dists = np.zeros((num_test, num_train))
+
+    n_bins = int(X.max() + 1)
+
+    X = np.apply_along_axis(lambda x: np.histogram(
+        x, density=True, bins=n_bins, range=(0, n_bins))[0], 1, X)
+    X_train = np.apply_along_axis(lambda x: np.histogram(
+        x, density=True, bins=n_bins, range=(0, n_bins))[0], 1, self.X_train)
+
+    X += 0.000001
+    X_train += 0.000001
+
+    X = np.asarray(X)
+    X_train = np.asarray(X_train)
+
+    for i in range(num_test):
+      dists[i, :] = - np.sum((X[i] * np.log2(X_train)), axis=1)
+
+    return dists
+
+  def compute_distances_l2norm(self, X):
     """
     Compute the distance between each test point in X and each training point
     in self.X_train using no explicit loops.
@@ -147,25 +181,11 @@ class KNearestNeighbor(object):
     num_test = X.shape[0]
     num_train = self.X_train.shape[0]
     dists = np.zeros((num_test, num_train))
-    #########################################################################
-    # TODO:                                                                 #
-    # Compute the l2 distance between all test points and all training      #
-    # points without using any explicit loops, and store the result in      #
-    # dists.                                                                #
-    #                                                                       #
-    # You should implement this function using only basic array operations; #
-    # in particular you should not use functions from scipy.                #
-    #                                                                       #
-    # HINT: Try to formulate the l2 distance using matrix multiplication    #
-    #       and two broadcast sums.                                         #
-    #########################################################################
     # expand equation (x-y)^2 = x^2 + y^2 - 2xy
     dists = np.sum(X**2, axis=1, keepdims=True) + np.sum(self.X_train**2, axis=1) \
         - 2 * np.matmul(X, self.X_train.T)
     dists = np.sqrt(dists)
-    #########################################################################
-    #                         END OF YOUR CODE                              #
-    #########################################################################
+
     return dists
 
   def predict_labels(self, dists, k=1):
@@ -187,24 +207,7 @@ class KNearestNeighbor(object):
       # A list of length k storing the labels of the k nearest neighbors to
       # the ith test point.
       closest_y = []
-      #########################################################################
-      # TODO:                                                                 #
-      # Use the distance matrix to find the k nearest neighbors of the ith    #
-      # testing point, and use self.y_train to find the labels of these       #
-      # neighbors. Store these labels in closest_y.                           #
-      # Hint: Look up the function numpy.argsort.                             #
-      #########################################################################
       closest_y = self.y_train[np.argsort(dists[i])][0:k]
-      #########################################################################
-      # TODO:                                                                 #
-      # Now that you have found the labels of the k nearest neighbors, you    #
-      # need to find the most common label in the list closest_y of labels.   #
-      # Store this label in y_pred[i]. Break ties by choosing the smaller     #
-      # label.                                                                #
-      #########################################################################
       y_pred[i] = np.bincount(closest_y).argmax()
-      #########################################################################
-      #                           END OF YOUR CODE                            #
-      #########################################################################
 
     return y_pred
--- iWERS/knn.py
+++ iWERS/knn.py
@@ -1,253 +1,179 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
-from skimage.feature import local_binary_pattern
-import os
-from skimage.util import img_as_float
+
 from scipy import ndimage as ndi
 from skimage.filters import gabor_kernel
+from skimage.feature import local_binary_pattern
+
+# from skimage.util import img_as_float
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from classifiers import KNearestNeighbor
 
-as_gray = False
 
 plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-rootdir = "./data/atex/"
-xatex = {}
-yatex = {}
 
-lbp = {}
-kmeans_clustering = {}
-kmeans_clustering["data"] = []
-kmeans_clustering["target"] = []
-
-if as_gray:
-    xatex["train"] = np.zeros([8753, 32, 32])
-    xatex["val"] = np.zeros([1252, 32, 32])
-    xatex["test"] = np.zeros([2498, 32, 32])
-else:
-    xatex["train"] = np.zeros([8753, 32, 32, 3])
-    xatex["val"] = np.zeros([1252, 32, 32, 3])
-    xatex["test"] = np.zeros([2498, 32, 32, 3])
-
-lbp["train"] = np.zeros([8753, 32, 32])
-lbp["val"] = np.zeros([1252, 32, 32])
-lbp["test"] = np.zeros([2498, 32, 32])
+def image_normalization(image):
+    # Normalize images separately across the three color channels.
+    mean = np.mean(image, axis=tuple(range(image.ndim - 1)))
+    std = np.std(image, axis=tuple(range(image.ndim - 1)))
+    return (image - mean) / std
 
 
-yatex["train"] = np.zeros(8753)
-yatex["val"] = np.zeros(1252)
-yatex["test"] = np.zeros(2498)
-
-
-METHOD = 'uniform'
-radius = 2
-
-n_points = 8 * radius
-
-
-sigma = 1
-theta = 1
-frequency = 0.3
-theta = (theta / 4.) * np.pi
-
-kernel = gabor_kernel(frequency, theta=theta, sigma_x=sigma, sigma_y=sigma)
-
-
-def power(image, kernel):
-    # Normalize images for better comparison.
+def kernel3C(gkernel):
     arrays = [kernel for _ in range(3)]
-    kernel3d = np.stack(arrays, axis=2)
-    # mean = np.mean(image, axis=tuple(range(image.ndim - 1)))
-    # std = np.std(image, axis=tuple(range(image.ndim - 1)))
-    image = img_as_float(image)
-    real_feature = ndi.convolve(image, np.real(kernel3d), mode='wrap')[:, :, 1]
-    imag_feature = ndi.convolve(image, np.imag(kernel3d), mode='wrap')[:, :, 1]
+    return np.stack(arrays, axis=2)
+
+
+as_gray = True
+def power(image, kernel, norm=True, as_gray=as_gray):
+
+    if norm:
+        image = image_normalization(image)
+    if as_gray:
+        real_feature = ndi.convolve(image, np.real(kernel), mode='wrap')
+        imag_feature = ndi.convolve(image, np.imag(kernel), mode='wrap')
+    else:
+        kernel = kernel3C(kernel)
+        real_feature = ndi.convolve(
+            image, np.real(kernel), mode='wrap')[:, :, 1]
+        imag_feature = ndi.convolve(
+            image, np.imag(kernel), mode='wrap')[:, :, 1]
+
     return np.sqrt(real_feature**2 + imag_feature**2)
 
 
-atex_sets = ["train", "val", "test"]
+def dataloader(dataset, as_gray=True, rootdir="./data/atex/"):
 
-for path in atex_sets:
-    atexdir = os.path.join(rootdir, path)
-    counter = 0
-    idx = 0
-    for root, dirs, files in os.walk(atexdir, topdown=True):
-        # print(counter)
-        for image in files:
-            # print(idx)
-            if image.endswith(".jpg"):
-                xatex[path][idx] = img_as_float(
-                    io.imread(os.path.join(root, image), as_gray=as_gray))
-                lbp[path][idx] = power(xatex[path][idx], kernel)
-                # lbp[path][idx] = local_binary_pattern(xatex[path][idx], n_points, radius, METHOD)
-                kmeans_clustering["data"].append(
-                    np.reshape(lbp[path][idx], -1))
-                kmeans_clustering["target"].append(counter - 1)
-                yatex[path][idx] = counter - 1
-                idx += 1
-        counter += 1
+    dataset = {
+        "train": {"data": [], "target": []},
+        "test": {"data": [], "target": []},
+        "val": {"data": [], "target": []}
+    }
 
-# x = np.asarray(kmeans_clustering["data"])
+    data_sets = ["train", "val", "test"]
+    for set_ in data_sets:
+        datadir = os.path.join(rootdir, set_)
+        counter = 0
+        idx = 0
+        for root, dirs, files in os.walk(datadir, topdown=True):
+            for image in files:
+                if image.endswith(".jpg"):
+                    dataset[set_]["data"].append(
+                        io.imread(os.path.join(root, image), as_gray=as_gray))
+                    dataset[set_]["target"].append(counter - 1)
+                    idx += 1
+            counter += 1
+        dataset[set_]["data"] = np.asarray(dataset[set_]["data"])
+        dataset[set_]["target"] = np.asarray(dataset[set_]["target"])
 
-pca = PCA(n_components=100, random_state=88)
-pca.fit(kmeans_clustering["data"])
-x = pca.transform(kmeans_clustering["data"])
-
-plt.scatter(x[:, 0], x[:, 1], c=kmeans_clustering["target"])
-plt.show()
+    return dataset
 
 
-inertia_list = []
-for k in range(2, 16):
-    kmn = KMeans(n_clusters=k, random_state=88)
-    kmn.fit(x)
-    pred = kmn.labels_
-    print(
-        np.sum(pred == kmeans_clustering["target"]) / len(kmeans_clustering["target"]))
-    inertia_list.append(kmn.inertia_)
+############################# ANALYSIS #############################
+# visualization of patches
+# atex = dataloader("atex", as_gray=False)
 
-# print(np.sum(pred == kmeans_clustering["target"]) / len(kmeans_clustering["target"]))
-
-plt.plot(np.arange(2, 16), inertia_list, 'o--')
-plt.show()
-exit()
-
-X_train = lbp["train"]
-# y_train = yatex["train"].astype(int) - 1
-X_val = lbp["val"]
-# y_val = yatex["val"].astype(int) - 1
-X_test = lbp["test"]
-# y_test = yatex["test"].astype(int) - 1
-
-classes = ['pool', 'flood', 'hot_spring', 'waterfall', 'lake', 'snow', 'rapids',
-           'river', 'glaciers', 'puddle', 'sea', 'delta', 'estuary', 'wetland', 'swamp']
-num_classes = len(classes)
+# classes = ['pool', 'flood', 'hot_spring', 'waterfall', 'lake', 'snow', 'rapids',
+#            'river', 'glaciers', 'puddle', 'sea', 'delta', 'estuary', 'wetland', 'swamp']
+# num_classes = len(classes)
 
 # samples_per_class = 7
 # for y, cls in enumerate(classes):
-#     idxs = np.flatnonzero(y_train == y)
+#     idxs = np.flatnonzero(atex["train"]["target"] == y)
 #     idxs = np.random.choice(idxs, samples_per_class, replace=False)
 #     for i, idx in enumerate(idxs):
 #         plt_idx = i * num_classes + y + 1
 #         plt.subplot(samples_per_class, num_classes, plt_idx)
-#         plt.imshow(X_train[idx]) # .astype('uint8'))
+#         plt.imshow(atex["train"]["data"][idx].astype('uint8'))
 #         plt.axis('off')
 #         if i == 0:
 #             plt.title(cls)
 # plt.show()
 
+# PCA to visualize first two eigenvectors of train data
+# atex = dataloader("atex", as_gray=True)
+# data = atex["train"]["data"].reshape(atex["train"]["data"].shape[0], -1)
 
+# pca = PCA(n_components=100, random_state=88)
+# pca.fit(data)
+# x = pca.transform(data)
+
+# fig, axes = plt.subplots(nrows=1, ncols=1)
+# axes.scatter(x[:, 0], x[:, 1], c=atex["train"]["target"])
+# plt.show()
+
+# KMeans to evaluate the inetria
+# inertia_list = []
+# for k in range(2, 16):
+#     kmn = KMeans(n_clusters=k, random_state=88)
+#     kmn.fit(x)
+#     pred = kmn.labels_
+#     inertia_list.append(kmn.inertia_)
+
+# fig, axes = plt.subplots(nrows=1, ncols=1)
+# axes.plot(np.arange(2, 16), inertia_list, 'o--')
+# plt.show()
+
+# KNN analysis
+atex = dataloader("atex", as_gray=as_gray)
+# as_gray=True --> results are around 20%
+# as_gray=False --> results are around 10%
+# preprocessing works! TODO: check the results with normalization
+X_train = atex["train"]["data"]
+y_train = atex["train"]["target"]
+X_val = atex["val"]["data"]
+y_val = atex["val"]["target"]
+
+# gabor analysis
+sigma = 1
+theta = (1 / 4.) * np.pi
+frequency = 0.3
+kernel = gabor_kernel(frequency, theta=theta, sigma_x=sigma, sigma_y=sigma)
+
+X_train = map(lambda x: power(x, kernel), X_train)
+X_train = np.asarray(list(X_train))
+
+X_val = map(lambda x: power(x, kernel), X_val)
+X_val = np.asarray(list(X_val))
+
+# lbp analysis
+METHOD = 'uniform'
+radius = 2
+n_points = 8 * radius
+
+X_train = map(lambda x: local_binary_pattern(
+    x, n_points, radius, METHOD), X_train)
+X_train = np.asarray(list(X_train))
+
+X_val = map(lambda x: local_binary_pattern(
+    x, n_points, radius, METHOD), X_val)
+X_val = np.asarray(list(X_val))
+
+#####################################
 X_train = np.reshape(X_train, (X_train.shape[0], -1))
 X_val = np.reshape(X_val, (X_val.shape[0], -1))
-# print(X_train.shape, X_val.shape)
 
-
-# def kullback_leibler_divergence(p, q):
-#     p = np.asarray(p)
-#     q = np.asarray(q)
-#     filt = np.logical_and(p != 0, q != 0)
-#     return np.sum(p[filt] * np.log2(p[filt] / q[filt]))
-
-
-# def kullback_leibler_divergencev(p, q):
-#     p = np.asarray(p)
-#     q = np.asarray(q)
-#     return np.sum(p * np.log2(p / q))
-
-
-# n_bins = int(X_test.max() + 1)
-
-# X = np.apply_along_axis(lambda x: np.histogram(
-#     x, density=True, bins=n_bins, range=(0, n_bins))[0], 1, X_test)
-
-# import time
-
-# dists = np.zeros(X.shape[0])
-# since = time.time()
-# for i in range(X.shape[0]):
-#     dists[i] = kullback_leibler_divergence(X[0], X[i])
-# print(time.time() - since)
-# print(dists.shape)
-# print(dists[:20])
-
-# X += 0.000001
-# X = np.asarray(X)
-
-# since = time.time()
-# dists2 = np.sum(X[0] * np.log2(X[0] / X), axis=1)
-# print(time.time() - since)
-# print(dists2[:20])
-
-# exit()
-
-from classifiers import KNearestNeighbor
-
-# Create a kNN classifier instance.
-# Remember that training a kNN classifier is a noop:
-# the Classifier simply remembers the data and does no further processing
 classifier = KNearestNeighbor()
-# classifier.train(X_train, y_train)
-
-# dists = classifier.compute_distances_no_loops(X_val)
-# dists = classifier.compute_distances_two_loops(X_val)
-
-
-# num_folds = 5
 k_choices = [1, 3, 5, 8, 15, 50, 70, 100]
-
-# X_train_folds = []
-# y_train_folds = []
-################################################################################
-# TODO:                                                                        #
-# Split up the training data into folds. After splitting, X_train_folds and    #
-# y_train_folds should each be lists of length num_folds, where                #
-# y_train_folds[i] is the label vector for the points in X_train_folds[i].     #
-# Hint: Look up the numpy array_split function.                                #
-################################################################################
-# X_train_folds = np.array_split(X_train, num_folds)
-# y_train_folds = np.array_split(y_train, num_folds)
-################################################################################
-#                                 END OF YOUR CODE                             #
-################################################################################
-
-# A dictionary holding the accuracies for different values of k that we find
-# when running cross-validation. After running cross-validation,
-# k_to_accuracies[k] should be a list of length num_folds giving the different
-# accuracy values that we found when using that value of k.
 k_to_accuracies = {}
 
-
-################################################################################
-# TODO:                                                                        #
-# Perform k-fold cross validation to find the best value of k. For each        #
-# possible value of k, run the k-nearest-neighbor algorithm num_folds times,   #
-# where in each case you use all but one of the folds as training data and the #
-# last fold as a validation set. Store the accuracies for all fold and all     #
-# values of k in the k_to_accuracies dictionary.                               #
-################################################################################
 for k in k_choices:
     k_to_accuracies[k] = []
-    # for i in range(num_folds):
-    # prepare training data for the current fold
-    # X_train_fold = np.concatenate([ fold for j, fold in enumerate(X_train_folds) if i != j ])
-    # y_train_fold = np.concatenate([ fold for j, fold in enumerate(y_train_folds) if i != j ])
 
     # use of k-nearest-neighbor algorithm
     classifier.train(X_train, y_train)
-    y_pred = classifier.predict(X_val, k=k, num_loops=2)
+    y_pred = classifier.predict(X_val, k=k, method=0)
 
     # Compute the fraction of correctly predicted examples
     num_correct = np.sum(y_pred == y_val)
     accuracy = float(num_correct) / X_val.shape[0]
     k_to_accuracies[k].append(accuracy)
-
-################################################################################
-#                                 END OF YOUR CODE                             #
-################################################################################
 
 # Print out the computed accuracies
 for k in sorted(k_to_accuracies):
