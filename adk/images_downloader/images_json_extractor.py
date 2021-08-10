be44abafc67b543b883b6344c50fd255be4d4ba4
smhassanerfani <serfani@email.sc.edu>
2021-08-10 11:26:55 -0400

    Modified input arguments.

--- adk/images_downloader/images_json_extractor.py
+++ adk/images_downloader/images_json_extractor.py
@@ -3,20 +3,20 @@ import json
 import argparse
 
 
-IMAGES_ROOT = "/home/serfani/Downloads/json_project/images"
-LABEL_DIR = "/home/serfani/Downloads/json_project/"
-JSON_DIR = "/home/serfani/Downloads/json_project/"
+IMAGES_DIR = "/home/serfani/Downloads/atlantis/s1a/breakwater/images"
+LICENSE_DIRS = "/home/serfani/Downloads/atlantis/s1a/breakwater/"
+JSON_DIR = "/home/serfani/Downloads/atlantis/s1a/breakwater/"
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(
         description="This code parses and captures the corresponding information of final images from initial json file and saves in new json file.")
-    parser.add_argument("-i", "--images-root", type=str, default=IMAGES_ROOT, required=False,
-                        help="The path of final images.")
-    parser.add_argument("-l", "--label-dir", type=str, default=LABEL_DIR, required=False,
-                        help="The path of directory including the first images based on different CC license code.")
+    parser.add_argument("-i", "--images-dir", type=str, default=IMAGES_DIR, required=False,
+                        help="The path of final annotated images.")
+    parser.add_argument("-l", "--license-dirs", type=str, default=LICENSE_DIRS, required=False,
+                        help="The path of 8 license directories including the primary requested images.")
     parser.add_argument("-j", "--json-dir", type=str, default=JSON_DIR, required=False,
-                        help="The path the reparsed json will be dumped.")
+                        help="The path that the reparsed json will be dumped.")
 
     return parser.parse_args()
 
@@ -24,10 +24,10 @@ def get_arguments():
 args = get_arguments()
 
 
-def get_images_list(image_root):
-    image_root = os.path.join(image_root, "images")
+def get_images_list(images_dir):
+    images_dir = os.path.join(images_dir, "images")
     images_list = list()
-    for root, dirs, files in os.walk(image_root, topdown=True):
+    for root, dirs, files in os.walk(images_dir, topdown=True):
         for file in files:
             if file.endswith(".jpg"):
                 images_list.append(file.split(".")[0])
@@ -35,9 +35,9 @@ def get_images_list(image_root):
     return images_list
 
 
-def get_image_info(label_dir, images_list):
+def get_image_info(license_dirs, images_list):
     json_list = list()
-    for root, dirs, files in os.walk(label_dir, topdown=True):
+    for root, dirs, files in os.walk(license_dirs, topdown=True):
         for file in files:
             if file.endswith(".json"):
                 with open(os.path.join(root, file), ) as jf:
@@ -79,7 +79,7 @@ def dump_json_info(json_root, json_list):
 def main():
 
     json_list = get_image_info(
-        args.label_dir, get_images_list(args.images_root))
+        args.license_dirs, get_images_list(args.images_dir))
     dump_json_info(args.json_dir, json_list)
     print(f"number of images were parsed: {len(json_list)}")
 
