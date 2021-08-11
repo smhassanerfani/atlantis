eb45ea5b2ada5e09a60b818889f3927c390b559b
smhassanerfani <serfani@email.sc.edu>
2021-08-11 16:25:37 -0400

    Modified image organizer dataloader.

--- adk/images_organizer/dataloader.sh
+++ adk/images_organizer/dataloader.sh
@@ -1,30 +1,36 @@
 #! /bin/bash
 
 # origin directory & sub directories
-odir=/home/serfani/Downloads/atlantis/s1n;
-subdirs=$(echo $(ls ${odir}));
+odir=/home/serfani/Downloads/atlantis_raw/s1n;
 
 # destination directory
 ddir=/home/serfani/Downloads/atlantis_new;
 
 
-sdirs=("delta" "estuary" "flood" "glaciers")
-ntrains=(1155 439 989 844)
-nvals=(165 63 141 121)
-ntests=(330 125 283 240)
+sdirs=("cliff" "cypress_tree" "fjord" "flood");
+ntrains=(28 28 62 81);
+nvals=(5 4 9 12);
+ntests=(8 8 17 23);
 
 for i in "${!sdirs[@]}"; do 
-	echo $i
+	echo $i;
 	sdir=${sdirs[$i]}; ntrain=${ntrains[$i]}; nval=${nvals[$i]}; ntest=${ntests[$i]};
-	find ${odir}/${sdir}/*.jpg | cut -d "/" -f 7 | shuf > ${odir}/${sdir}_images.txt
+	find ${odir}/${sdir}/images/*.jpg | cut -d "/" -f 9 | shuf > ${odir}/${sdir}/images_list.txt;
+	cat ${odir}/${sdir}/images_list.txt | cut -d "." -f 1 | sed 's/$/.png/' > ${odir}/${sdir}/masks_list.txt;
 	
-	cat ${odir}/${sdir}_images.txt | head -n ${ntrain} > ${odir}/${sdir}_train_images.txt
-	cat ${odir}/${sdir}_images.txt | tail -n ${ntest} > ${odir}/${sdir}_test_images.txt
-	cat ${odir}/${sdir}_images.txt | head -n -${ntest}| tail -n ${nval} > ${odir}/${sdir}_val_images.txt
+	cat ${odir}/${sdir}/images_list.txt | head -n ${ntrain} > ${odir}/${sdir}/images_train_list.txt; 
+	cat ${odir}/${sdir}/images_list.txt | tail -n ${ntest} > ${odir}/${sdir}/images_test_list.txt; 
+	cat ${odir}/${sdir}/images_list.txt | head -n -${ntest} | tail -n ${nval} > ${odir}/${sdir}/images_val_list.txt;
 
-	rsync --files-from=${odir}/${sdir}_train_images.txt ${odir}/${sdir} ${ddir}/train/${sdir}
-	rsync --files-from=${odir}/${sdir}_test_images.txt ${odir}/${sdir} ${ddir}/test/${sdir}
-	rsync --files-from=${odir}/${sdir}_val_images.txt ${odir}/${sdir} ${ddir}/val/${sdir}
+	cat ${odir}/${sdir}/masks_list.txt | head -n ${ntrain} > ${odir}/${sdir}/masks_train_list.txt; 
+	cat ${odir}/${sdir}/masks_list.txt | tail -n ${ntest} > ${odir}/${sdir}/masks_test_list.txt;
+	cat ${odir}/${sdir}/masks_list.txt | head -n -${ntest} | tail -n ${nval} > ${odir}/${sdir}/masks_val_list.txt; 
 
-
+	rsync --files-from=${odir}/${sdir}/images_train_images.txt ${odir}/${sdir}/images ${ddir}/images/train/${sdir};
+	rsync --files-from=${odir}/${sdir}/images_test_images.txt ${odir}/${sdir}/images ${ddir}/images/test/${sdir};
+	rsync --files-from=${odir}/${sdir}/images_val_images.txt ${odir}/${sdir}/images ${ddir}/images/val/${sdir};
+	rsync --files-from=${odir}/${sdir}/masks_train_images.txt ${odir}/${sdir}/masks ${ddir}/masks/train/${sdir};
+	rsync --files-from=${odir}/${sdir}/masks_test_images.txt ${odir}/${sdir}/masks ${ddir}/masks/test/${sdir};
+	rsync --files-from=${odir}/${sdir}/masks_val_images.txt ${odir}/${sdir}/masks ${ddir}/masks/val/${sdir};
+done
 
