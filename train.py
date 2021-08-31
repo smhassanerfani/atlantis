23ed7fdfbc9f32b2abd5831e567bb64d5acfdf47
smhassanerfani <serfani@email.sc.edu>
2021-08-31 15:57:57 -0400

    Removed Redundant pl learning func.

--- train.py
+++ train.py
@@ -64,17 +64,6 @@ def get_arguments():
 args = get_arguments()
 
 
-# def lr_poly(base_lr, iter, max_iter, power):
-#     return base_lr * ((1 - float(iter) / max_iter) ** (power))
-
-
-# def adjust_learning_rate(optimizer, i_iter, total_steps):
-#     lr = lr_poly(args.learning_rate, i_iter, total_steps, args.power)
-#     optimizer.param_groups[0]['lr'] = lr
-#     if len(optimizer.param_groups) > 1:
-#         optimizer.param_groups[1]['lr'] = lr * 10
-
-
 def lr_poly(base_lr, iter, max_iter, power):
     return base_lr * ((1 - float(iter) / max_iter) ** (power))
 
