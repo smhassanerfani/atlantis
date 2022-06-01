10c413195c9405460fbf6acb0bf3510821a3364c
Mohammad Erfani <50107399+smhassanerfani@users.noreply.github.com>
2022-06-01 08:51:26 -0400

    Update dataloader.sh

--- adk/images_organizer/dataloader.sh
+++ adk/images_organizer/dataloader.sh
@@ -18,19 +18,22 @@ for i in "${!sdirs[@]}"; do
 	find ${odir}/${sdir}/images/*.jpg | cut -d "/" -f 9 | shuf > ${odir}/${sdir}/images_list.txt;
 	cat ${odir}/${sdir}/images_list.txt | cut -d "." -f 1 | sed 's/$/.png/' > ${odir}/${sdir}/masks_list.txt;
 	
+	
 	cat ${odir}/${sdir}/images_list.txt | head -n ${ntrain} > ${odir}/${sdir}/images_train_list.txt; 
 	cat ${odir}/${sdir}/images_list.txt | tail -n ${ntest} > ${odir}/${sdir}/images_test_list.txt; 
 	cat ${odir}/${sdir}/images_list.txt | head -n -${ntest} | tail -n ${nval} > ${odir}/${sdir}/images_val_list.txt;
 
 	cat ${odir}/${sdir}/masks_list.txt | head -n ${ntrain} > ${odir}/${sdir}/masks_train_list.txt; 
 	cat ${odir}/${sdir}/masks_list.txt | tail -n ${ntest} > ${odir}/${sdir}/masks_test_list.txt;
-	cat ${odir}/${sdir}/masks_list.txt | head -n -${ntest} | tail -n ${nval} > ${odir}/${sdir}/masks_val_list.txt; 
+	cat ${odir}/${sdir}/masks_list.txt | head -n -${ntest} | tail -n ${nval} > ${odir}/${sdir}/masks_val_list.txt;
+	
 
-	rsync --files-from=${odir}/${sdir}/images_train_images.txt ${odir}/${sdir}/images ${ddir}/images/train/${sdir};
-	rsync --files-from=${odir}/${sdir}/images_test_images.txt ${odir}/${sdir}/images ${ddir}/images/test/${sdir};
-	rsync --files-from=${odir}/${sdir}/images_val_images.txt ${odir}/${sdir}/images ${ddir}/images/val/${sdir};
-	rsync --files-from=${odir}/${sdir}/masks_train_images.txt ${odir}/${sdir}/masks ${ddir}/masks/train/${sdir};
-	rsync --files-from=${odir}/${sdir}/masks_test_images.txt ${odir}/${sdir}/masks ${ddir}/masks/test/${sdir};
-	rsync --files-from=${odir}/${sdir}/masks_val_images.txt ${odir}/${sdir}/masks ${ddir}/masks/val/${sdir};
+	rsync --files-from=${odir}/${sdir}/images_train_list.txt ${odir}/${sdir}/images ${ddir}/images/train/${sdir};
+	rsync --files-from=${odir}/${sdir}/images_test_list.txt ${odir}/${sdir}/images ${ddir}/images/test/${sdir};
+	rsync --files-from=${odir}/${sdir}/images_val_list.txt ${odir}/${sdir}/images ${ddir}/images/val/${sdir};
+	
+	rsync --files-from=${odir}/${sdir}/masks_train_list.txt ${odir}/${sdir}/masks ${ddir}/masks/train/${sdir};
+	rsync --files-from=${odir}/${sdir}/masks_test_list.txt ${odir}/${sdir}/masks ${ddir}/masks/test/${sdir};
+	rsync --files-from=${odir}/${sdir}/masks_val_list.txt ${odir}/${sdir}/masks ${ddir}/masks/val/${sdir};
 done
 
